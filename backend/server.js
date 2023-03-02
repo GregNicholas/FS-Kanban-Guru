48377b5641b28608f61f2f0fc3aa9c1f3cd31f1f
Greg Schoenberg <gregschoenberg@gmail.com>
2023-03-01 18:47:08 -0800

    edit changing frontend from build to dist

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,9 @@ app.use(errorHandler)
 
 // Serve the frontend
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../', 'frontend', 'build', 'index.html')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../', 'frontend', 'dist', 'index.html')))
 } else {
     app.get('/', (req, res) => res.send('Please set to production'))
 }

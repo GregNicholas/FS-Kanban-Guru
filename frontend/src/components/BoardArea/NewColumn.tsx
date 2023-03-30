6afed84ae445b2be05aceb9b93682163aa8326a0
Greg Schoenberg <gregschoenberg@gmail.com>
2023-03-29 18:25:34 -0700

    push new build

--- frontend/src/components/BoardArea/NewColumn.tsx
+++ frontend/src/components/BoardArea/NewColumn.tsx
@@ -6,7 +6,7 @@ const NewColumn = ({ addColumn }:NewColumnProps) => {
   return (
     <div 
       onClick={addColumn}
-      className="flex items-center justify-center cursor-pointer text-m-gray hover:text-main-purple bg-[#E9EFFA] dark:bg-d-gray w-70 rounded-lg mt-10 mb-7 mr-48"
+      className="flex transition-all items-center justify-center cursor-pointer text-m-gray hover:text-main-purple bg-[#E9EFFA] dark:bg-d-gray w-70 rounded-lg mt-10 mb-7 mr-48"
     >
       <p className="font-bold text-2xl">+ New Column</p>
     </div>
